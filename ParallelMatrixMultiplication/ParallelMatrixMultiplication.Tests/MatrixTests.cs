4308ef0790d7417528bb454c882748b76f5b5a55
kirillbenga <kirillbenga@gmail.com>
2025-09-18 11:50:52 +0300

    did some of the tests

---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication.Tests/MatrixTests.cs
@@ -0,0 +1,80 @@
+﻿namespace ParallelMatrixMultiplication.Tests;
+
+public class MatrixTests
+{
+    private readonly int[,] matrix1 = Matrix.ReadFromFile("../../../TestInput1.txt");
+    private readonly int[,] matrix2 = Matrix.ReadFromFile("../../../TestInput2.txt");
+    private int[,] resultOfParallelMultiplication = Matrix.ParallelMultiplication(matrix1, matrix2);
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TheMatricesAreEqualTest()
+    {
+        var matrix1 = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+        };
+        var matrix2 = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+        };
+        var matrix3 = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+        };
+
+        Assert.That(Matrix.Equals(matrix1, matrix1), Is.True);
+        Assert.That(Matrix.Equals(matrix1, matrix2), Is.False);
+        Assert.That(Matrix.Equals(matrix2, matrix3), Is.True);
+        Assert.That(Matrix.Equals(matrix3, matrix1), Is.False);
+    }
+
+    [Test]
+    public void ReadFromFileTest()
+    {
+        var sample1 = new int[,]
+        {
+            { 1, 2, 3, 4, 5 },
+            { 6, 7, 8, 9, 10 },
+            { 11, 12, 13, 14, 15 },
+        };
+        var sample2 = new int[,]
+        {
+            { 1, 2 },
+            { 3, 4 },
+            { 5, 6 },
+            { 7, 8 },
+            { 9, 10 },
+        };
+        var testMatrix1 = Matrix.ReadFromFile("../../../TestInput1.txt");
+        var testMatrix2 = Matrix.ReadFromFile("../../../TestInput2.txt");
+        Assert.Multiple(() =>
+        {
+            Assert.That(Matrix.Equals(testMatrix1, sample1), Is.True);
+            Assert.That(Matrix.Equals(testMatrix2, sample2), Is.True);
+        });
+    }
+
+    [Test]
+    public void ReadFromFileShouldThrowEmptyFileExceptionIfFileIsEmpty()
+    {
+        Assert.Throws<EmptyFileException>(
+            () => Matrix.ReadFromFile("../../../TestInputFailed1.txt"));
+    }
+
+    [Test]
+    public void ReadFromFileShouldThrowInvalidDataExceptionIfIncorrectMatrixstructure()
+    {
+        Assert.Throws<InvalidDataException>(
+            () => Matrix.ReadFromFile("../../../TestInputFailed2.txt"));
+    }
+}
--- ParallelMatrixMultiplication/ParallelMatrixMultiplication/Matrix.cs
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/Matrix.cs
@@ -191,6 +191,34 @@ public static class Matrix
         return matrix;
     }
 
+    /// <summary>
+    /// Check the equality of the matrices.
+    /// </summary>
+    /// <param name="matrix1">The first matrix is for comparison.</param>
+    /// <param name="matrix2">The second matrix is for comparison.</param>
+    /// <returns>True if the values are equal.</returns>
+    public static bool Equals(int[,] matrix1, int[,] matrix2)
+    {
+        if ((matrix1.GetLength(0) != matrix2.GetLength(0)) ||
+            (matrix1.GetLength(1) != matrix2.GetLength(1)))
+        {
+            return false;
+        }
+
+        for (var i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix1.GetLength(1); j++)
+            {
+                if (matrix1[i, j] != matrix2[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     private static void MultiplicationKernel(int[,] matrix1, int[,] matrix2T, int[,] result, int startRow, int endRow)
     {
         ArgumentOutOfRangeException.ThrowIfNegative(startRow);
