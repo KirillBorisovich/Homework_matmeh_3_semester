fa7a44f560d2e0e339a99c4b0878933282348d7a
kirillbenga <kirillbenga@gmail.com>
2025-11-24 12:04:23 +0300

    feat: added console UI

--- MyNUnit/MyNUnit/Program.cs
+++ MyNUnit/MyNUnit/Program.cs
@@ -2,4 +2,23 @@
 // Copyright (c) Bengya Kirill under MIT License.
 // </copyright>
 
-Console.WriteLine("Hello, World!");
+using MyNUnit;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Usage: program <the path to the file or directory>\n");
+    return;
+}
+
+var inputPath = args[0];
+
+try
+{
+    var result = await new MyNUnitClass().RunAllTheTestsAlongThisPath(inputPath);
+    Console.WriteLine(result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message + "\n");
+    return;
+}
