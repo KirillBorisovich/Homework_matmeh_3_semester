5e71c4968fa37db006a6cfd6fe2f8431dd8ad059
kirillbenga <kirillbenga@gmail.com>
2025-12-23 15:17:10 +0300

    feat: did task

---
+++ MyNUnitWebSolution/MyNUnitWebSolution/Pages/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyNUnitWebSolution.Pages;
+
+public class index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Test_23_12_2025/Test_23_12_2025/Program.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Program.cs" company="Bengya Kirill">
+// Copyright (c) Bengya Kirill under MIT License.
+// </copyright>
+
+using Test_23_12_2025;
+
+Console.WriteLine("Online chat");
+
+try
+{
+    using var consoleStream = Console.OpenStandardInput();
+    var chat = new OnlineChat(consoleStream);
+    switch (args.Length)
+    {
+        case 1:
+            _ = chat.StartServer(int.Parse(args[0]));
+            break;
+        case 2:
+            _ = chat.StartClient(args[0], int.Parse(args[1]));
+            break;
+        default:
+            Console.WriteLine("Incorrect input of arguments");
+            break;
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message + "\n");
+}
